cb0d71dfaf98f16f2e7c57fbb816480a15d295d0
dolied <3552086889@qq.com>
2025-04-19 15:32:25 +0800

    后端修改
    与数据库连接

--- back/src/main/java/com/example/service/AdminService.java
+++ back/src/main/java/com/example/service/AdminService.java
@@ -1,15 +1,26 @@
 package com.example.service;
-
+import com.example.entity.Admin;
+import com.example.mapper.AdminMapper;
+import jakarta.annotation.Resource;
 import com.example.exception.CustomException;
 import org.springframework.stereotype.Service;
-
+import java.util.List;
 @Service
 public class AdminService {
+
+    @Resource
+    AdminMapper adminMapper;
+
+
     public String admin(String name){
         if(name.equals("admin")){
             return "admin";
         } else {
             throw new CustomException("账号错误");
         }
+    }
+
+    public List<Admin> selectAll(){
+        return adminMapper.selectAll();
     }
 }
