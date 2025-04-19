e0a36e85d0d4f67c486b38e5a25be5190d6a50a6
dolied <3552086889@qq.com>
2025-04-19 10:40:39 +0800

    前端安装axios，修改初始跳转页面

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -4,6 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {path: '/', redirect: '/Manager'},
     {path: '/manager',name: 'home',component: import('../views/Manager.vue'),
       children: [
         {path: 'home',component: import('../views/Home.vue')}
